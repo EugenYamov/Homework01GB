8571b282f13789cc2badc6d715f81cafdd9a6b54
Yamov_Eugen <e.yamow@mail.ru>
2022-07-09 20:08:46 +0300

    Task 2 is complit

---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Число А больше, чем B");
+}
+
+if (numberA == numberB)
+{
+    Console.WriteLine("Числа равны");
+}
+
+else
+{
+    Console.WriteLine("Число B больше, чем А");
+}
