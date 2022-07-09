61f04d4befa8b8d13dc95edf14d3dec3fd2e3228
Yamov_Eugen <e.yamow@mail.ru>
2022-07-09 20:56:04 +0300

    Task 4 is complit

---
+++ Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число С");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB && numberA > numberC)
+    Console.WriteLine($"Число {numberA} больше");
+
+else if (numberB > numberA && numberB > numberC)
+    Console.WriteLine($"Число {numberB} больше");
+
+else if (numberC > numberB && numberC > numberA)
+    Console.WriteLine($"Число {numberC} больше");
+
+else
+    Console.WriteLine("Чиcла равны");
+
