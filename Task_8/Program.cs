be624983770cd4f46f05e41fb308bc18c8525aae
Yamov_Eugen <e.yamow@mail.ru>
2022-07-09 21:16:38 +0300

    Task 8 is complit

---
+++ Task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите произвольное целое число (N)");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Все четные числа от 1 до N:");
+
+for (int i = 0; i <= N; i++)
+{
+    if (i % 2 == 0)
+        Console.WriteLine(i);
+}
